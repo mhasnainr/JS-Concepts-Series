3f83f2d2ec2fa97fa1d807d3b0527f537c972b85
Hasnain <mhasnainr.415@gmail.com>
2024-11-18 17:36:52 +0500

    js concepts series

---
+++ day-1/index.js
@@ -0,0 +1,73 @@
+// Q1
+
+// console.log("Weloome to Day 1");
+
+// Q2
+
+// let a = 3;
+// let b = 5;
+// let result = a + b;
+// console.log(`Result of a + b is: ${result}`);
+
+// Q3
+
+// let num = -50;
+
+// if (num % 2 === 0) {
+//   console.log(`${num} is an even number`);
+// } else {
+//   console.log(`${num} is an odd number`);
+// }
+
+// Q4
+
+// let num1 = 4;
+// let num2 = 6;
+
+// if (num1 > num2) {
+//   console.log(`${num1} is greater than ${num2}`);
+// } else if (num2 > num1) {
+//   console.log(`${num2} is greater than ${num1}`);
+// } else if (num1 === num2) {
+//   console.log(`${num1} and ${num2} are both equal`);
+// }
+
+// Q5
+
+// let celsius = 12;
+// let fahrenheit = celsius * 1.8 + 32;
+
+// console.log(`Temperature: ${celsius}°C = ${fahrenheit}°F`);
+
+// Q6: find total price
+
+// let item1 = 234;
+// let item2 = 431;
+// let item3 = 545;
+
+// let totalPrice = item1 + item2 + item3;
+
+// console.log(`Total Price: ${totalPrice}`);
+
+// Q7: tip for a bill
+
+// let bill = 1430;
+// let tip = bill * 0.15;
+
+// console.log(`Tip: ${tip}`);
+
+// Q8
+
+// let name = prompt("Enter your name");
+// alert(`Welcome to JS world, ${name}!`);
+
+// Q9: display current year
+// Topic: Objects (yet to learn)
+
+// let currentDate = new Date();
+// let currentYear = currentDate.getFullYear();
+
+// document.getElementById("year").innerHTML = currentYear;
+
+
+// 
