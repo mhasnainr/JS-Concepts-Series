6b74b4d09c92240b1c6a9f1b8ac9335a4a1466ea
Hasnain <mhasnainr.415@gmail.com>
2024-11-20 19:47:41 +0500

    day-3: loop Qs

---
+++ day-3/index.js
@@ -0,0 +1,42 @@
+// Topic: Loop (remaining Qs)
+
+// L3: Q1- prime number finder
+
+// for (let num = 2; num <= 50; num++) {
+//   let isPrime = true;
+
+//   for (let i = 2; i <= Math.sqrt(num); i++) {
+//     if (num % i === 0) {
+//       isPrime = false;
+//       break;
+//     }
+//   }
+//   if (isPrime) {
+//     console.log(num);
+//   }
+// }
+
+// L3: Q2- reverse a string:
+
+// let word = "hello";
+// let reversedWord = "";
+
+// for (let i = word.length - 1; i >= 0; i--) {
+//   reversedWord += word[i];
+// }
+// console.log(reversedWord);
+
+// L3: Q3- fibonacci sequence:
+
+// let a = 3;
+// let b = 5;
+
+// console.log(a);
+// console.log(b);
+
+// for (let i = 3; i <= 10; i++) {
+//   let next = a + b;
+//   console.log(next);
+//   a = b;
+//   b = next;
+// }
